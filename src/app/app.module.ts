574d662be1776847286f1abbcca7d8d00cfa88e9
Brian <punreliable@gmail.com>
2022-09-09 13:39:57 -0400

    Added Stlyesheet and header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteNavbarComponent } from './site-navbar/site-navbar.component';
+import { SiteFooterComponent } from './site-footer/site-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteNavbarComponent,
+    SiteFooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/site-navbar/site-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-site-navbar',
+  templateUrl: './site-navbar.component.html',
+  styleUrls: ['./site-navbar.component.scss']
+})
+export class SiteNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
